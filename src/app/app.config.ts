651ff426953502ef86847daed15df9a76890fc9b
asitha <kaushaladikari@gmail.com>
2026-04-23 13:46:31 +0530

    update config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
 import { JwtInterceptor } from './auth/jwt.interceptor';
@@ -11,6 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimations(),
     provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(FormsModule),   // ✅ Enable ngModel
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
   ]
 };
